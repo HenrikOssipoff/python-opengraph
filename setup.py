b6f3cf6204224dfa578a0808f475e6f877077143
HenrikOssipoff <henrik.ossipoff@gmail.com>
2015-06-15 20:40:29 +0200

    Bump version for next release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, unicode_literals
 
 from setuptools import setup, find_packages
 
-VERSION = (0, 0, 2)
+VERSION = (0, 0, 3)
 __version__ = VERSION
 __versionstr__ = '.'.join([str(v) for v in VERSION])
 
